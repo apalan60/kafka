84670091abff313eb76cdda395b59a2ed3261af6
Hong-Yi Chen <apalan60@gmail.com>
2025-06-04 01:12:55 +0800

    MINOR: Refactor brokerContactTimesMs from AtomicLong to Long

--- metadata/src/main/java/org/apache/kafka/controller/metrics/QuorumControllerMetrics.java
+++ metadata/src/main/java/org/apache/kafka/controller/metrics/QuorumControllerMetrics.java
@@ -80,7 +80,7 @@ public class QuorumControllerMetrics implements AutoCloseable {
     private final AtomicLong operationsStarted = new AtomicLong(0);
     private final AtomicLong operationsTimedOut = new AtomicLong(0);
     private final AtomicLong newActiveControllers = new AtomicLong(0);
-    private final Map<Integer, AtomicLong> brokerContactTimesMs = new ConcurrentHashMap<>();
+    private final Map<Integer, Long> brokerContactTimesMs = new ConcurrentHashMap<>();
     private final int sessionTimeoutMs;
 
     private Consumer<Long> newHistogram(MetricName name, boolean biased) {
@@ -160,7 +160,7 @@ public class QuorumControllerMetrics implements AutoCloseable {
     }
 
     public void addTimeSinceLastHeartbeatMetric(int brokerId) {
-        brokerContactTimesMs.put(brokerId, new AtomicLong(time.milliseconds()));
+        brokerContactTimesMs.put(brokerId, time.milliseconds());
         registry.ifPresent(r -> r.newGauge(
             getBrokerIdTagMetricName(
                 "KafkaController",
@@ -267,8 +267,7 @@ public class QuorumControllerMetrics implements AutoCloseable {
     }
 
     public void updateBrokerContactTime(int brokerId) {
-        AtomicLong contactTime = brokerContactTimesMs.computeIfAbsent(brokerId, k -> new AtomicLong());
-        contactTime.set(time.milliseconds());
+        brokerContactTimesMs.compute(brokerId, (id, timeMs) -> time.milliseconds());
     }
 
     public int timeSinceLastHeartbeatMs(int brokerId) {
@@ -276,7 +275,7 @@ public class QuorumControllerMetrics implements AutoCloseable {
             return sessionTimeoutMs;
         } else {
             return Math.min(
-                (int) (time.milliseconds() - brokerContactTimesMs.get(brokerId).get()),
+                (int) (time.milliseconds() - brokerContactTimesMs.get(brokerId)),
                 sessionTimeoutMs
             );
         }
